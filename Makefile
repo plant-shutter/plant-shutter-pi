de9b401512972bb158ac473fc51add4b02e6a15d
Vincent <857618786vin@gmail.com>
2023-05-29 16:21:53 +0800

    test

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
-build_local:
-	go build -o bin/plant-shutter main.go
-
-build_test:
-	go build -o bin/plant-shutter cmd/test/main.go
+build:
+	@docker build --platform "linux/arm/v6" --output "./bin"  .
--- cmd/test/main.go
+++ cmd/test/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/vladimirvivien/go4vl/device"
 	"github.com/vladimirvivien/go4vl/v4l2"
-	"os"
+
 	"plant-shutter-pi/pkg/utils"
 )
 
@@ -15,18 +18,22 @@ var (
 
 func main() {
 	sizes := []Size{
-		{
-			Width:  640,
-			Height: 480,
-		},
-		{
-			Width:  1920,
-			Height: 1080,
-		},
-		{
-			Width:  2048,
-			Height: 1080,
-		},
+		//{
+		//	Width:  640,
+		//	Height: 480,
+		//},
+		//{
+		//	Width:  1280,
+		//	Height: 720,
+		//},
+		//{
+		//	Width:  1920,
+		//	Height: 1080,
+		//},
+		//{
+		//	Width:  2560,
+		//	Height: 1440,
+		//},
 		{
 			Width:  3280,
 			Height: 2464,
@@ -36,6 +43,7 @@ func main() {
 		if err := shot(s.Width, s.Height); err != nil {
 			panic(err)
 		}
+		time.Sleep(time.Second * 5)
 	}
 }
 
@@ -45,6 +53,7 @@ type Size struct {
 }
 
 func shot(width, height int) error {
+	log.Printf("shot %d*%d", width, height)
 	dev, err := device.Open(
 		devName,
 		device.WithPixFormat(v4l2.PixFormat{PixelFormat: v4l2.PixelFmtRGB24, Width: uint32(width), Height: uint32(height)}),
@@ -52,25 +61,50 @@ func shot(width, height int) error {
 	if err != nil {
 		return err
 	}
-	defer dev.Close()
+	defer func(dev *device.Device) {
+		err := dev.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}(dev)
+
+	pixFormat, err := dev.GetPixFormat()
+	if err != nil {
+		return err
+	}
+	log.Println(pixFormat)
+	pixFormat, err = v4l2.GetPixFormat(dev.Fd())
+	if err != nil {
+		return err
+	}
+	log.Println(pixFormat)
 
 	// start stream
 	if err = dev.Start(context.TODO()); err != nil {
 		return err
 	}
-	frame := <-dev.GetOutput()
+	for i := 0; i < 10; i++ {
+		//t1 := time.Now()
+		frame := <-dev.GetOutput()
+		//t2 := time.Now()
+		img := utils.DecodeRGB(frame, int(pixFormat.Width), int(pixFormat.Height))
+		//t3 := time.Now()
+		if err = utils.EncodeJPEGFile(img, fmt.Sprintf("%d-%d.jpg", width, height), 95); err != nil {
+			return err
+		}
+		//t4 := time.Now()
 
-	err = os.WriteFile(fmt.Sprintf("%d-%d.raw", width, height), frame, 0640)
-	if err != nil {
-		return err
+		//d1 := t2.Sub(t1)
+		//d2 := t3.Sub(t2)
+		//d3 := t4.Sub(t3)
+		//dA := t4.Sub(t1)
+		//log.Println(d1, d2, d3, dA)
 	}
 
-	img := utils.DecodeRGB(frame, width, height)
-	fmt.Println("DecodeYUYV.end")
-
-	if err = utils.EncodeJPEGFile(img, fmt.Sprintf("%d-%d.jpg", width, height), 95); err != nil {
-		return err
-	}
+	//err = os.WriteFile(fmt.Sprintf("%d-%d.raw", width, height), frame, 0640)
+	//if err != nil {
+	//	return err
+	//}
 
 	return nil
 }
